e4258f34dce5cc744510d3b645b65647c5e5b2ab
Alfred Nelson <alfrednelson375@gmail.com>
2021-11-02 18:42:24 +0530

    Added git hooks, eslint, prettier and rubocop

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
 
-    validates :email, presence: true, uniqueness: { case_sensitive: false }, format: {  with: VALID_EMAIL_REGEX }
-    validates :last_name, presence: true, length: { maximum: Constants::MAX_USER_NAME_LENGTH }
-    validates :first_name, presence: true, length: { maximum: Constants::MAX_USER_NAME_LENGTH }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  validates :last_name, presence: true, length: { maximum: Constants::MAX_USER_NAME_LENGTH }
+  validates :first_name, presence: true, length: { maximum: Constants::MAX_USER_NAME_LENGTH }
 
-    before_save :to_lower_case
+  before_save :to_lower_case
 
-    private
+  private
 
-        def to_lower_case
-            email.downcase!
-        end
+    def to_lower_case
+      email.downcase!
+    end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,10 +1,13 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(email: 'oliver@example.com',
-                    first_name: 'oliver',
-                    last_name: 'twist'
+    @user = User.new(
+      email: "oliver@example.com",
+      first_name: "oliver",
+      last_name: "twist"
                 )
   end
 
@@ -14,19 +17,19 @@ class UserTest < ActiveSupport::TestCase
   end
 
   def test_first_name_should_be_present
-    @user.first_name = ''
+    @user.first_name = ""
     assert_not @user.valid?
     assert_includes @user.errors.full_messages, "First name can't be blank"
   end
 
   def test_last_name_should_be_present
-    @user.last_name = ''
+    @user.last_name = ""
     assert_not @user.valid?
     assert_includes @user.errors.full_messages, "Last name can't be blank"
   end
-  
+
   def test_email_should_be_present
-    @user.email = ''
+    @user.email = ""
     assert_not @user.valid?
     assert_includes @user.errors.full_messages, "Email can't be blank"
   end
@@ -45,7 +48,7 @@ class UserTest < ActiveSupport::TestCase
     @user.save!
     some_user = @user.dup
     assert_not some_user.valid?
-    assert_includes some_user.errors.full_messages, 'Email has already been taken'
+    assert_includes some_user.errors.full_messages, "Email has already been taken"
   end
 
   def test_upcase_email_should_not_be_saved_when_downcase_is_already_present
@@ -59,22 +62,23 @@ class UserTest < ActiveSupport::TestCase
   def test_email_should_be_lowercase
     @user.email = @user.email.upcase!
     assert @user.valid?
-    assert_equal @user.email , @user.email.downcase!
+    assert_equal @user.email, @user.email.downcase!
   end
 
   def test_email_address_should_reject_valid_address
-    invalid_emails = %w[user@example,com user_at_example.org user.name@example. @sam-sam.com sam@sam+exam.com fishy+#.com]
+    invalid_emails = %w[user@example,com user_at_example.org user.name@example. @sam-sam.com sam@sam+exam.com
+fishy+#.com]
     invalid_emails.each do |email|
-    @user.email = email
-    assert @user.invalid?
+      @user.email = email
+      assert @user.invalid?
     end
   end
 
   def test_email_address_should_accept_valid_address
     valid_emails = %w[user@example.com USER@example.COM US-ER@example.org first.last@example.in user+one@example.ac.in]
     valid_emails.each do |email|
-    @user.email = email
-    assert @user.valid?
+      @user.email = email
+      assert @user.valid?
     end
   end
 end
