689ee7b9a090984d0825ac8248fec94004609deb
Alfred Nelson <alfrednelson375@gmail.com>
2021-11-02 12:46:50 +0530

    Removed fixtures

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module QuizzyByAlfredNelson
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    config.generators do |g|
+      g.test_framework :test_unit, fixture: false
+    end
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
