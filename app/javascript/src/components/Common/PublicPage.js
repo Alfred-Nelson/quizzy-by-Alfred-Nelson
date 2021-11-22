db6b42888a394d8ba099daf1a6b23baec14d158f
Alfred Nelson <alfrednelson375@gmail.com>
2021-11-22 21:55:05 +0530

    Added loading to a page

--- app/javascript/src/components/Common/PublicPage.js
+++ app/javascript/src/components/Common/PublicPage.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from "react";
 
-import { Typography } from "@bigbinary/neetoui/v2";
+import { Typography, PageLoader } from "@bigbinary/neetoui/v2";
 import { Redirect, useParams } from "react-router";
 
 import { QuizApi } from "apis/quiz";
 
 const PublicPage = () => {
   const [quizName, setQuizName] = useState("");
+  const [loading, setLoading] = useState(false);
   const { slug } = useParams();
 
   const fetchDetails = async () => {
+    setLoading(true);
     const response = await QuizApi.getQuizBySlug(slug);
     setQuizName(response.data.name);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -20,12 +23,18 @@ const PublicPage = () => {
 
   return (
     <div>
-      {quizName != "" ? (
-        <Redirect to={`/public/${slug}/attempt/new`} />
+      {loading ? (
+        <PageLoader />
       ) : (
-        <div className="w-full flex justify-center h-64 mt-20 items center">
-          <Typography> 👹 Please Enter an Existing slug </Typography>
-        </div>
+        <>
+          {quizName != "" ? (
+            <Redirect to={`/public/${slug}/attempt/new`} />
+          ) : (
+            <div className="w-full flex justify-center h-64 mt-20 items center">
+              <Typography> 👹 Please Enter an Existing slug </Typography>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
